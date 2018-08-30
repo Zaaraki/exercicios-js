0f689106f3b43920ba434e268e9996d3f48b142d
Amaro <amaro.euclides@gmail.com>
2018-08-30 10:42:52 -0300

    Adicionado Funcao 2

--- fundamentos/funcao2.js
+++ fundamentos/funcao2.js
@@ -18,3 +18,6 @@ console.log(soma(2, 3))
 
 const subtracao = (a, b) => a-b //vai executar apenas uma sentença de código
 console.log(subtracao(2, 3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('cool')
