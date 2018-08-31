5bd2b4ba7482bc444111a238722deff2e92d2089
Amaro <amaro.euclides@gmail.com>
2018-08-31 12:51:02 -0300

    Loop com var

---
+++ fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,8 @@
+for(var i=0; i<10; i++){
+    console.log(i)
+}
+
+console.log('i= ', i)
+
+//var não tem escopo de bloco, então o i está visível dentro e fora do bloco
+// i=10, pois é o valor que fez ele sair do laço
